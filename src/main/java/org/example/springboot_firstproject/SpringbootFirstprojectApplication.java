0a8c56ff7efb4cc91a99f824f32423cfc7a137db
GabrielaCigerova <gabriela.cigerova@le-campus-numerique.fr>
2024-12-13 11:06:58 +0100

    Initial commit

---
+++ src/main/java/org/example/springboot_firstproject/SpringbootFirstprojectApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboot_firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFirstprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFirstprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springboot_firstproject/SpringbootFirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot_firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirstprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
