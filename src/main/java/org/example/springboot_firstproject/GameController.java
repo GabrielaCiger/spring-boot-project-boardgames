0baab688f0d1a4cbfdd22c57d1371b592ce70546
GabrielaCigerova <gabriela.cigerova@le-campus-numerique.fr>
2024-12-16 16:43:43 +0100

    I added and tested DELETE method

--- src/main/java/org/example/springboot_firstproject/GameController.java
+++ src/main/java/org/example/springboot_firstproject/GameController.java
@@ -2,14 +2,21 @@ package org.example.springboot_firstproject;
 
 import fr.le_campus_numerique.square_games.engine.Game;
 import fr.le_campus_numerique.square_games.engine.GameFactory;
+import fr.le_campus_numerique.square_games.engine.GameStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 @RestController
-public class GameCatalogController {
+public class GameController {
 
     @Autowired
     private GameCatalog gameCatalog;
@@ -27,9 +34,28 @@ public class GameCatalogController {
         return newGame.getId().toString();
     }
 
-    @GetMapping("games/{gameId}")
+    @GetMapping("/games/{gameId}")
     public Object getGame(@PathVariable String gameId) {
         return games.stream().filter(game -> game.getId().toString().equals(gameId)).findFirst();
     }
 
+    @GetMapping("/games/ongoing")
+    public List<Map<String, String>> getOngoingGames() {
+        return games.stream()
+                .filter(game -> game.getStatus() == GameStatus.ONGOING)
+                .map(game -> Map.of("id", game.getId().toString(), "game", game.getFactoryId()))
+                .collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/games/delete/{gameId}")
+    public ResponseEntity<String> deleteGame(@PathVariable String gameId) {
+        boolean isDeleted = games.removeIf(game -> game.getId().toString().equals(gameId));
+
+        if (isDeleted) {
+            return ResponseEntity.ok("Game deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND) //404
+                    .body("Game with ID " + gameId + " not found");
+        }
+    }
 }
