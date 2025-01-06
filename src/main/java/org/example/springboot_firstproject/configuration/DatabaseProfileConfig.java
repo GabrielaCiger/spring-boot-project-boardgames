1d48d1c136382f48c806fab596ffe5ed4f5b46b0
GabrielaCigerova <gabriela.cigerova@le-campus-numerique.fr>
2025-01-06 16:41:11 +0100

    Started working on H2 - separated active profiles, how to do the config file?

---
+++ src/main/java/org/example/springboot_firstproject/configuration/DatabaseProfileConfig.java
@@ -0,0 +1,14 @@
+package org.example.springboot_firstproject.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class DatabaseProfileConfig {
+//
+//    @Bean
+//    @Profile("h2")
+//
+
+}
